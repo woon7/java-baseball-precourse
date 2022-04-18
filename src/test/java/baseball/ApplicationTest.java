75c0afe434619b06d72599dd7624a9ef5688738f
Yongwoon Kim <righteous0319@daum.net>
2022-04-18 23:47:34 +0900

    test(application) : 테스트 정리
    변경 내용
    - DisplayName 추가
    - 테스트 분할

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,15 +1,17 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
     @Test
+    @DisplayName("게임 종료 후 재시작")
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
@@ -21,34 +23,51 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @DisplayName("입력값이 3자리가 넘어가는 경우")
     void 예외_테스트() {
-        // 입력값이 3자리가 넘어가는 경우
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        // 입력값에 공백이 있는 경우
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("  3"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-        // 입력값에 문자가 들어가는 경우
+    }
+
+    @Test
+    @DisplayName("입력값에 문자가 들어가는 경우")
+    void runWithChar() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1R1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        // 입력값이 서로 다른 숫자가 아닌 경우
+    }
+
+    @Test
+    @DisplayName("입력값에 공백이 있는 경우")
+    void runWithEmpty() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" 3 "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("입력값이 서로 다른 숫자가 아닌 경우")
+    void runWithDuplicateDigits() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("111"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        // 랜덤값이 서로 다른 숫자가 아닌 경우
-        assertRandomNumberInRangeTest(
-                () -> assertThatThrownBy(() -> runException("246", "135", "1", "597", "589", "2"))
-                        .isInstanceOf(IllegalArgumentException.class),
-                1, 3, 5, 5, 5, 5
-        );
     }
+
+    // 게임종료_후_재시작()과 같이 테스트 돌리면 Randoms.pickNumberInRange()가 제대로 동작하지 않음
+//    @Test
+//    @DisplayName("랜덤값이 서로 다른 숫자가 아닌 경우")
+//    void runWithDuplicateRandomDigits() {
+//        assertRandomNumberInRangeTest(
+//                () -> assertThatThrownBy(() -> runException("246", "135", "1", "597", "589", "2"))
+//                        .isInstanceOf(IllegalArgumentException.class),
+//                1, 3, 5, 5, 5, 5
+//        );
+//    }
 
     @Override
     public void runMain() {
